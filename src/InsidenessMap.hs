8a8c97b969ec42eff5e07e5129fc3dba85b88407
lane <lane@downstairspeople.org>
2006-06-23 14:27:14 -0400

    work on OneToMany map
    darcs-hash:20060623182714-7cce2-9bd5c70b869b04806977d59386c667f28996cee2.gz

--- src/InsidenessMap.hs
+++ src/InsidenessMap.hs
@@ -1,17 +1,43 @@
 module OneToMany
     where
 
+import Data.Map as Map
+
 data OneToMany a b c = 
     OneToMany
     (Map a [b])
     (Map b (a,c))
 
-otmPut :: (a,b,c) -> OnetoMany a b c -> OneToMany a b c
-otmPut (a,b,c) (OneToMany ab bac) =
+empty :: OneToMany a b c
+empty = OneToMany (Map.empty) (Map.empty)
+
+insert :: (a,b,c) -> OneToMany a b c -> OneToMany a b c
+insert (a,b,c) (OneToMany _ bac) =
     let old_parent = lookup b bac
 	clean_map = if (isNothing old_parent)
 		    then bac
 		    else adjust (delete b) (fst $ just old_parent)
-    OneToMany
-    (insertWith (++) a [b] clean_map)
-    (insert b (a,c) bac)
+        in OneToMany
+               (insertWith (++) a [b] clean_map)
+               (insert b (a,c) bac)
+
+parent :: b -> OneToMany a b c -> Maybe a
+parent b (OneToMany ab bac) = fst $ lookup b bac
+
+children :: a -> OneToMany a b c -> Maybe [(a,b,c)]
+children the_parent (OneToMany ab bac) =
+    let the_children = lookup a ab
+        in if (isNothing the_children)
+           then []
+           else zip3 (
+                      repeat a,
+                      the_children,
+                      map (\x -> snd $ bac !! x) the_children
+                     )
+
+toList :: OneToMany a b c -> [(a,b,c)]
+toList (OneToMany ab bac) = foldr1 (++) (map children (keys ab))
+
+fromList :: [(a,b,c)] -> OneToMany a b c
+fromList lst = foldr insert empty lst
+
