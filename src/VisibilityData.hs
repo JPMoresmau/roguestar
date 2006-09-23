b502a081627dafc15c8dee86529528fe1fef86e0
lane <lane@downstairspeople.org>
2006-09-23 08:39:15 -0400

    moved facing from VisibilityData to Facing.hs
    darcs-hash:20060923123915-7cce2-fe2cad7c01d6afac5a1cd65ccf8f3d0f1fb753b3.gz

--- src/VisibilityData.hs
+++ src/VisibilityData.hs
@@ -31,44 +31,6 @@ module VisibilityData
 import TerrainData
 import Data.List
 
-data Facing = North
-	    | NorthEast
-	    | East
-	    | SouthEast
-	    | South
-	    | SouthWest
-	    | West
-	    | NorthWest
-	    | Here
-	      deriving (Eq,Enum,Bounded)
--- |
--- In relative coordinates, one integral step in the specified direction.
---
-facingToRelative :: Facing -> (Integer,Integer)
-facingToRelative North = (0,1)
-facingToRelative NorthEast = (1,1)
-facingToRelative East = (1,0)
-facingToRelative SouthEast = (1,-1)
-facingToRelative South = (0,-1)
-facingToRelative SouthWest = (-1,-1)
-facingToRelative West = (-1,0)
-facingToRelative NorthWest = (-1,1)
-facingToRelative Here = (0,0)
-
--- |
--- In relative coordinates, roughly seven integral steps in the specified direction.
--- Note: 7 is a small integer such that the square root of 5^2 is quite close to 7.
---
-facingToRelative7 :: Facing -> (Integer,Integer)
-facingToRelative7 North = (0,7)
-facingToRelative7 NorthEast = (5,5)
-facingToRelative7 East = (7,0)
-facingToRelative7 SouthEast = (5,-5)
-facingToRelative7 South = (0,-7)
-facingToRelative7 SouthWest = (-5,-5)
-facingToRelative7 West = (-7,0)
-facingToRelative7 NorthWest = (-5,5)
-facingToRelative7 Here = (0,0)
 
 -- |
 -- We multiply a creature's hide check by this number if it is standing on this terrain.
