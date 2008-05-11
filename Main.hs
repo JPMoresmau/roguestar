a51b512b3df555436e9ffa935d38d7203f2b4cb3
gwern0 <gwern0@gmail.com>
2008-05-11 19:30:09 -0400

    convert src/roguestar-gl to Main.hs
    darcs-hash:20080511233009-f7719-fc0508cdc99556085420aebcc6fabf05e48795bf.gz

---
+++ Main.hs
@@ -0,0 +1,17 @@
+module Main (main) where
+{- This is a small shell wrapper.
+   What we do is we use the 'netpipes' program to set up a socket, and then we
+   tell the shell to set the roguestar server running; we pause briefly to give
+   it a chance to set up the socket. Then we open up the nice GL
+   interface.
+   The 'sleep 2' bit is ugly; we need it because if either one opens up and
+   can't find the socket at 5618, it'll just exit - it won't wait for the other
+   one to start up. It's a race condition, alas. -}
+import System.Cmd (system)
+
+main :: IO ()
+main =  do system $ "hose localhost 5618 --out --in --unix " ++ "roguestar-engine version over begin" ++ " &"
+           system "sleep 2" -- TODO: Figure out some in-Haskell way
+           system $ "faucet 5618 --out --in --unix --once " ++ "roguestar-gl-bin" ++ " &"
+           return ()
+
