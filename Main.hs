1b73834b0f38a887e33363f113f53106217df613
Christopher Lane Hinson <lane@downstairspeople.org>
2008-06-02 16:50:36 -0400

    More debugging information for launch script.
    darcs-hash:20080602205036-2da90-1a062c003e76c26012028d0931ba9e2d2403ceda.gz

--- Main.hs
+++ Main.hs
@@ -4,14 +4,25 @@ module Main (main) where
    tell the shell to set the roguestar server running; we pause briefly to give
    it a chance to set up the socket. Then we open up the nice GL
    interface.
-   The 'sleep 2' bit is ugly; we need it because if either one opens up and
+   The 'threadDelay' bit is ugly; we need it because if either one opens up and
    can't find the socket at 5618, it'll just exit - it won't wait for the other
    one to start up. It's a race condition, alas. -}
 import System.Cmd (system)
+import System.Exit
+import Control.Concurrent
 
 main :: IO ()
-main =  do system $ "faucet 5618 --out --in --unix --once " ++ "roguestar-engine version over begin" ++ " &"
-           system "sleep 2" -- TODO: Figure out some in-Haskell way
-           system $ "hose localhost 5618 --out --in --unix " ++ "roguestar-gl" ++ " &"
+main =  do forkIO $ 
+               do putStrLn "Launching engine..."
+                  ec <- system $ "faucet 5618 --out --in --unix --once " ++ "roguestar-engine version over begin"
+                  case ec of
+                      ExitFailure x -> putStrLn $ "roguestar-engine terminated unexpectedly (" ++ show x ++ ")"
+                      _ -> return ()
+                  return ()
+           threadDelay 2000000
+           putStrLn "Launching OpenGL client..."
+           ec <- system $ "hose localhost 5618 --out --in --unix " ++ "roguestar-gl"
+           case ec of
+               ExitFailure x -> putStrLn $ "roguestar-gl terminated unexpectedly (" ++ show x ++ ")"
+               _ -> return()
            return ()
-
