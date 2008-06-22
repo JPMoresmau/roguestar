bd77915f57db16704944314b8752723cccbb8fbf
Christopher Lane Hinson <lane@downstairspeople.org>
2008-06-21 22:09:06 -0400

    Portable launcher, possible windows candidate.
    darcs-hash:20080622020906-2da90-0e0d2ee567c706c935d542300df7a8d2ed3d8d9d.gz

--- Main.hs
+++ Main.hs
@@ -1,28 +1,22 @@
 module Main (main) where
-{- This is a small shell wrapper.
-   What we do is we use the 'netpipes' program to set up a socket, and then we
-   tell the shell to set the roguestar server running; we pause briefly to give
-   it a chance to set up the socket. Then we open up the nice GL
-   interface.
-   The 'threadDelay' bit is ugly; we need it because if either one opens up and
-   can't find the socket at 5618, it'll just exit - it won't wait for the other
-   one to start up. It's a race condition, alas. -}
-import System.Cmd (system)
+
+import System.Process
 import System.Exit
 import Control.Concurrent
+import System.Environment
 
 main :: IO ()
-main =  do forkIO $ 
-               do putStrLn "Launching engine..."
-                  ec <- system $ "faucet 5618 --out --in --unix --once " ++ "roguestar-engine version over begin"
-                  case ec of
-                      ExitFailure x -> putStrLn $ "roguestar-engine terminated unexpectedly (" ++ show x ++ ")"
-                      _ -> return ()
-                  return ()
-           threadDelay 2000000
-           putStrLn "Launching OpenGL client..."
-           ec <- system $ "hose localhost 5618 --out --in --unix " ++ "roguestar-gl"
-           case ec of
-               ExitFailure x -> putStrLn $ "roguestar-gl terminated unexpectedly (" ++ show x ++ ")"
-               _ -> return()
-           return ()
+main = 
+    do args <- getArgs
+       (input,out,err,roguestar_engine) <- runInteractiveProcess "roguestar-engine" ["version", "over", "begin"] 
+                                           Nothing Nothing
+       roguestar_gl <- runProcess "roguestar-gl" args Nothing Nothing (Just out) (Just input) Nothing
+       roguestar_gl_exit <- waitForProcess roguestar_gl
+       case roguestar_gl_exit of
+           ExitFailure x -> putStrLn $ "roguestar-gl terminated unexpectedly (" ++ show x ++  ")"
+           _ -> return ()
+       roguestar_engine_exit <- waitForProcess roguestar_engine
+       case roguestar_engine_exit of
+           ExitFailure x -> putStrLn $ "roguestar-engine terminated unexpectedly (" ++ show x ++ ")"
+           _ -> return ()
+
