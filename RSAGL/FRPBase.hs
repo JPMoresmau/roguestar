869b463982dd22079ce859d4551dbab9f26b1b6f
Christopher Lane Hinson <lane@downstairspeople.org>
2008-09-22 22:05:28 -0400

    FRPBase.lhs -> .hs

--- RSAGL/FRPBase.hs
+++ RSAGL/FRPBase.hs
@@ -1,21 +1,23 @@
-\section{RSAGL.FRPBase}
+{-# LANGUAGE Arrows, MultiParamTypeClasses, ExistentialQuantification, FlexibleInstances, Rank2Types #-}
 
-The FRPBase arrow is a composite of the Stateful and Threaded arrows intended to support functional reactive programming.
-
-\begin{code}
-{-# OPTIONS_GHC -fglasgow-exts -farrows #-}
-
+-- |  The 'FRPBase' arrow is a composite of the 'StatefulArrow' and 'ThreadedArrow' intended to support functional reactive programming.
 module RSAGL.FRPBase
     (FRPBase,
+     -- * Switching Operators
      RSAGL.FRPBase.switchContinue,
      RSAGL.FRPBase.switchTerminate,
+     -- * Threading Operators
      RSAGL.FRPBase.spawnThreads,
      RSAGL.FRPBase.killThreadIf,
      RSAGL.FRPBase.threadIdentity,
-     RSAGL.FRPBase.frpBaseContext,
+     -- * Embedding One FRPBase Instance in Another
+     RSAGL.FRPBase.frpBaseContext,     
+     -- * Embedding Explicit Underlying State
      RSAGL.FRPBase.withState,
      RSAGL.FRPBase.withExposedState,
+     -- * Embedding a StatefulArrow in an FRPBase Arrow
      RSAGL.FRPBase.statefulContext,
+     -- * The StatefulArrow form of an FRPBase Arrow
      RSAGL.FRPBase.statefulForm)
     where
 
@@ -26,11 +28,8 @@ import Control.Arrow.Transformer.State
 import RSAGL.StatefulArrow as StatefulArrow
 import RSAGL.ThreadedArrow as ThreadedArrow
 import RSAGL.ArrowTransformerOptimizer
-\end{code}
 
-FRPBase is a composite arrow in which the StatefulArrow is layered on top of the ThreadedArrow.
-
-\begin{code}
+-- | FRPBase is a composite arrow in which the StatefulArrow is layered on top of the ThreadedArrow.
 newtype FRPBase t i o a j p = FRPBase (
     ArrowTransformerOptimizer StatefulArrow (ThreadedArrow t i o a) j p)
 
@@ -62,72 +61,45 @@ killThreadIf = FRPBase $ lift ThreadedArrow.killThreadIf
 
 threadIdentity :: (ArrowChoice a) => FRPBase t i o a () t
 threadIdentity = FRPBase $ lift ThreadedArrow.threadIdentity
-\end{code}
 
-\subsection{Embedding one FRPBase instance in another}
 
-The frpBaseContext combinator allows a differently-typed FRPBase thread group to be embedded in another.  
-Using frpBaseContext, a thread can instantiate a group of threads that die whenever the calling thread 
-dies or switches.  That is, the thread group is part of the state of the calling thread.
-
-\begin{code}
+-- | The 'frpBaseContext' combinator allows a differently-typed 'FRPBase' thread group to be embedded in another.  
+-- Using 'frpBaseContext', a thread can instantiate a group of threads that die whenever the calling thread 
+-- dies or switches.  That is, the thread group is part of the state of the calling thread.
 frpBaseContext :: (Arrow a,ArrowChoice a,ArrowApply a) => 
     (forall x. j -> [(t,x)] -> [(t,(p,x))] -> [x]) ->
     [(t,FRPBase t j p a j p)] -> FRPBase u i o a j [(t,p)]
 frpBaseContext manageThreads threads = FRPBase $ raw $ statefulTransform lift (RSAGL.FRPBase.statefulForm manageThreads threads)
-\end{code}
 
-\subsection{Embedding explicit underlying state}
-
-The withState and withExposedState combinators are implemented in terms of the
-StatefulArrow combinators of the same name\footnote{See page \pageref{withState}}.
-
-\begin{code}
+-- | See 'RSAGL.StatefulArrow.withState'.
 withState :: (Arrow a,ArrowChoice a,ArrowApply a,Monoid p) => 
     (forall x. j -> [(t,x)] -> [(t,(p,x))] -> [x]) -> 
     [(t,FRPBase t j p (StateArrow s a) j p)] -> s -> FRPBase t i o a j [(t,p)]
 withState manageThreads threads s = FRPBase $ raw $ statefulTransform lift $ StatefulArrow.withState (RSAGL.FRPBase.statefulForm manageThreads threads) s
 
+-- | See 'RSAGL.StatefulArrow.withExposedState'
 withExposedState :: (Arrow a,ArrowChoice a,ArrowApply a,Monoid p) =>
     (forall x. j -> [(t,x)] -> [(t,(p,x))] -> [x]) -> 
     [(t,FRPBase t j p (StateArrow s a) j p)] -> FRPBase t i o a (j,s) ([(t,p)],s)
 withExposedState manageThreads threads = statefulContext $ StatefulArrow.withExposedState $ RSAGL.FRPBase.statefulForm manageThreads threads
-\end{code}
 
-\subsection{Embedding a StatefulArrow in an FRPBase arrow}
-\label{statefulContext}
-
-The statefulContext combinator allows a StatefulArrow to be embedded in an FRPBase, with
-the provision that the StatefulArrow does not have access to the threading operators.
-
-\begin{code}
+-- | The 'statefulContext' combinator allows a 'StatefulArrow' to be embedded in an 'FRPBase', with
+-- the provision that the 'StatefulArrow' does not have access to the threading operators.
 statefulContext :: (Arrow a,ArrowChoice a,ArrowApply a) =>
                        StatefulArrow a j p -> FRPBase t i o a j p
 statefulContext = FRPBase . raw . statefulTransform lift
-\end{code}
 
-\subsection{The StatefulArrow form of an FRPBase arrow}
-
-The FRPBase arrow can be made to appear as a StatefulArrow using statefulForm.
-\footnote{See \pageref{statefulForm}}
-
-\begin{code}
+-- | The 'FRPBase' arrow can be made to appear as a 'StatefulArrow' using statefulForm.
 statefulForm :: (Arrow a,ArrowChoice a,ArrowApply a) => 
      (forall x. i -> [(t,x)] -> [(t,(o,x))] -> [x]) -> 
      [(t,FRPBase t i o a i o)] -> StatefulArrow a i [(t,o)]
 statefulForm manageThreads = ThreadedArrow.statefulForm manageThreads . map (second $ statefulThread . fromFRPBase)
-\end{code}
 
-\subsection{Essential mechanism}
-
-The essential mechanism of the FRPBase arrow is the layering of a StatefulArrow over a ThreadedArrow,
-allowing threads that are both implicitly and explicitly stateful.  \texttt{statefulThread} implements this.
-In the event of an explicit switch, all implicit state is lost.
-
-\begin{code}
+-- | The essential mechanism of the FRPBase arrow is the layering of a StatefulArrow over a ThreadedArrow,
+-- allowing threads that are both implicitly and explicitly stateful.  \texttt{statefulThread} implements this.
+-- In the event of an explicit switch, all implicit state is lost.
 statefulThread :: (Arrow a,ArrowChoice a) => 
                   StatefulArrow (ThreadedArrow t i o a) i o -> ThreadedArrow t i o a i o
 statefulThread (StatefulArrow sf) = 
     proc b -> do (c,sf') <- sf -< b
                  ThreadedArrow.switchTerminate -< (Just $ statefulThread sf',c)
-\end{code}
