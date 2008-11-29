be480fcf9b3d0ae21614c5326e1793760c4a39af
Christopher Lane Hinson <lane@downstairspeople.org>
2008-11-29 09:27:57 -0500

    Slight rework of how guns are built from materials, increase variety of phase weapons.

--- src/ToolData.hs
+++ src/ToolData.hs
@@ -5,7 +5,9 @@ module ToolData
      gunThroughput,
      gunEndurance,
      toolName,
-     phase_pistol)
+     phase_pistol,
+     phaser,
+     phase_rifle)
     where
 
 import Substances
@@ -24,13 +26,18 @@ data Gun = Gun {
    gun_name :: String,
    gun_power_cell :: Chromalite,
    gun_substrate :: Material,
-   gun_casing :: Material,
    gun_medium :: Gas,
    gun_size :: GunSize }
      deriving (Eq,Read,Show)
 
 phase_pistol :: Tool
-phase_pistol = GunTool $ Gun "phase_pistol" Pteulanium Palladium Zinc Argon Pistol
+phase_pistol = GunTool $ Gun "phase_pistol" Pteulanium Zinc Argon Pistol
+
+phaser :: Tool
+phaser = GunTool $ Gun "phaser" Pteulanium Zinc Argon Carbine
+
+phase_rifle :: Tool
+phase_rifle = GunTool $ Gun "phase_rifle" Pteulanium Zinc Argon Rifle
 
 gunEnergyOutput :: Gun -> Integer
 gunEnergyOutput g = gunSizeClass g * (chromalitePotency $ gun_power_cell g)
@@ -40,12 +47,12 @@ gunThroughput g = ((material_critical_value $ materialValue $ gun_substrate g) +
                   (gasWeight $ gun_medium g)
 
 gunEndurance :: Gun -> Integer
-gunEndurance g = 10 * (material_construction_value $ materialValue $ gun_casing g)^2
+gunEndurance g = 10 * (material_construction_value $ materialValue $ gun_substrate g)^2
 
 gunSizeClass :: Gun -> Integer
 gunSizeClass (Gun { gun_size = Pistol }) = 1
 gunSizeClass (Gun { gun_size = Carbine}) = 3
-gunSizeClass (Gun { gun_size = Rifle}) = 4
+gunSizeClass (Gun { gun_size = Rifle}) = 5
 gunSizeClass (Gun { gun_size = Cannon}) = 7
 gunSizeClass (Gun { gun_size = Launcher}) = 10
 
