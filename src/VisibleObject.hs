f9d25cc67f05128cab44988858e6fddb1b8cc746
Christopher Lane Hinson <lane@downstairspeople.org>
2009-01-31 20:33:16 -0500

    VisibleObject.lhs -> .hs

--- src/VisibleObject.hs
+++ src/VisibleObject.hs
@@ -1,6 +1,3 @@
-\section{Visible Objects}
-
-\begin{code}
 {-# LANGUAGE Arrows #-}
 
 module VisibleObject
@@ -43,15 +40,18 @@ import Control.Monad
 data ToolThreadInput = ToolThreadInput {
     tti_wield_points :: Map.Map Integer CoordinateSystem }
 
+-- | Header function that just kills the current thread if it isn't a visible object.
 visibleObjectHeader :: RSAnimA (Maybe Integer) i o () ()
 visibleObjectHeader = proc () ->
     do unique_id <- arr (fromMaybe (error "visibleObjectHeader: threadIdentity was Nothing")) <<< threadIdentity -< ()
        uids <- allObjects -< ()
        killThreadIf -< isNothing $ find (== unique_id) uids
 
+-- | List all 'VisibleObject' records.
 visibleObjects :: RSAnimAX any t i o () [VisibleObject]
 visibleObjects = proc () -> arr (maybe [] tableSelectTyped) <<< sticky isJust Nothing <<< driverGetTableA -< ("visible-objects","0")
 
+-- | List all object UIDs.
 allObjects :: RSAnimAX any a i o () [Integer]
 allObjects = proc () ->
     do visible_object_ids <- arr (map vo_unique_id . maybe [] tableSelectTyped) <<< sticky isJust Nothing <<< driverGetTableA -< ("visible-objects","0")
@@ -79,33 +79,40 @@ isBeingWielded = wieldedParent >>> arr isJust
 visibleObjectUniqueID :: RSAnimA (Maybe Integer) i o () Integer
 visibleObjectUniqueID = arr (fromMaybe (error "visibleObjectUniqueID: threadIdentity was Nothing")) <<< threadIdentity
 
+-- | Get the 'VisibleObject' record for the current object.
 visibleObject :: RSAnimA (Maybe Integer) i o () (Maybe VisibleObject)
 visibleObject = proc () ->
     do unique_id <- visibleObjectUniqueID -< ()
        visible_objects <- visibleObjects -< ()
        returnA -< do find ((== unique_id) . vo_unique_id) visible_objects
 
+-- | Get an "object-details" field for the current visible object.
 objectDetailsLookup :: String -> RSAnimA (Maybe Integer) i o () (Maybe String)
 objectDetailsLookup field = proc _ ->
     do unique_id <- visibleObjectUniqueID -< ()
        m_details_table <- driverGetTableA -< ("object-details",show unique_id)
        sticky isJust Nothing -< (\x -> tableLookup x ("property","value") field) =<< m_details_table
 
+-- | Grid position to which the current object should move.
 objectDestination :: RSAnimA (Maybe Integer) i o () (Maybe (Integer,Integer))
 objectDestination = arr (fmap vo_xy) <<< visibleObject
-    
+
+-- | Correct position of the current object, with smooth translation.
 objectIdealPosition :: RSAnimA (Maybe Integer) i o () (Maybe Point3D)
 objectIdealPosition = 
     whenJust (approachA 0.25 (perSecond 3)) <<< 
     arr (fmap (\(x,y) -> Point3D (realToFrac x) 0 (negate $ realToFrac y))) <<< 
     objectDestination
 
+-- | Goal direction in which the current object should be pointed.
 objectFacing :: RSAnimA (Maybe Integer) i o () (Maybe BoundAngle)
 objectFacing = arr (fmap vo_facing) <<< visibleObject
 
+-- | Direction in which the current object should be pointed, with smooth rotation.
 objectIdealFacing :: RSAnimA (Maybe Integer) i o () (Maybe Angle)
 objectIdealFacing = arr (fmap unboundAngle) <<< whenJust (approachA 0.1 (perSecond 1)) <<< objectFacing
 
+-- | Combine 'objectIdealPosition' and 'objectIdealFacing' to place an object.
 objectIdealOrientation :: RSAnimA (Maybe Integer) i o () (Maybe CoordinateSystem)
 objectIdealOrientation = proc () ->
     do m_p <- objectIdealPosition -< ()
@@ -114,6 +121,8 @@ objectIdealOrientation = proc () ->
                      a <- m_a
 		     return $ translate (vectorToFrom p origin_point_3d) $ rotateY a $ root_coordinate_system
 
+-- | 'objectIdealOrientation' implementation that is aware of wield points for wieldable objects.  If an object is being
+-- wielded, it will snap to it's wield point.
 wieldableObjectIdealOrientation :: RSAnimA (Maybe Integer) i o ToolThreadInput (Maybe CoordinateSystem)
 wieldableObjectIdealOrientation = proc tti ->
     do ideal_resting <- objectIdealOrientation -< ()
@@ -121,4 +130,3 @@ wieldableObjectIdealOrientation = proc tti ->
        let wield_point = do wielded_parent <- m_wielded_parent
                             Map.lookup wielded_parent $ tti_wield_points tti
        returnA -< wield_point `mplus` ideal_resting
-\end{code}
