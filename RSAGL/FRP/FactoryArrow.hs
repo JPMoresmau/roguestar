63dc8a073f9a230cbc3e506e40f51c5b08ceceac
Christopher Lane Hinson <lane@downstairspeople.org>
2009-05-05 21:59:22 -0400

    Use type families to define the Monad associated with an Arrow by HasMonad.

--- RSAGL/FRP/FactoryArrow.hs
+++ RSAGL/FRP/FactoryArrow.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Arrows, MultiParamTypeClasses, FunctionalDependencies, TypeFamilies, FlexibleInstances #-}
+{-# LANGUAGE Arrows, MultiParamTypeClasses, TypeFamilies, FlexibleInstances, FlexibleContexts, UndecidableInstances #-}
 
 module RSAGL.FRP.FactoryArrow
     (FactoryArrow(..),
@@ -17,25 +17,25 @@ import Data.IORef
 import RSAGL.FRP.StatefulArrow
 
 -- | An 'Arrow' that generates another 'Arrow' inside a 'Monad'.
-newtype FactoryArrow m a i o = FactoryArrow { runFactory :: m (a i o) }
+newtype (HasMonad a) => FactoryArrow a i o = FactoryArrow { runFactory :: (MonadOf a) (a i o) }
 
-instance (Category a,Monad m) => Category (FactoryArrow m a) where
+instance (Category a,HasMonad a,Monad (MonadOf a)) => Category (FactoryArrow a) where
     (FactoryArrow a) . (FactoryArrow b) = FactoryArrow $
         do b' <- b
            a' <- a
            return $ a' . b'
     id = FactoryArrow $ return id
 
-instance (Arrow a,Monad m) => Arrow (FactoryArrow m a) where
+instance (Arrow a,HasMonad a,Monad (MonadOf a)) => Arrow (FactoryArrow a) where
     arr f = FactoryArrow $ return $ arr f
     first (FactoryArrow a) = FactoryArrow $ liftM first a
     second (FactoryArrow a) = FactoryArrow $ liftM second a
 
-instance (Monad m,Arrow a) => ArrowTransformer (FactoryArrow m) a where
+instance (Arrow a,HasMonad a,Monad (MonadOf a)) => ArrowTransformer FactoryArrow a where
     lift = FactoryArrow . return
 
 -- | Embed a stateful operation in an arrow.
-stateContext :: (Arrow a,Monad m,MonadReference m,HasMonad m a) => b -> a (b,i) (b,o) -> FactoryArrow m a i o
+stateContext :: (Arrow a,HasMonad a,Monad (MonadOf a),MonadReference (MonadOf a)) => b -> a (b,i) (b,o) -> FactoryArrow a i o
 stateContext initial_value actionA = FactoryArrow $
     do r <- newReference initial_value
        return $ proc i ->
@@ -45,7 +45,7 @@ stateContext initial_value actionA = FactoryArrow $
               returnA -< o
 
 -- | Generate a 'StatefulArrow' based on a 'FactoryArrow'.
-statefulForm :: (Arrow a,ArrowApply a,Monad m,MonadReference m,HasMonad m a) => FactoryArrow m a i o -> StatefulArrow a i o
+statefulForm :: (Arrow a,ArrowApply a,MonadReference (MonadOf a),HasMonad a) => FactoryArrow a i o -> StatefulArrow a i o
 statefulForm factory = StatefulArrow $ proc i ->
     do a <- monadicAction (\() -> runFactory factory) -< ()
        let actionA = proc i' -> 
@@ -54,10 +54,12 @@ statefulForm factory = StatefulArrow $ proc i ->
        app -< (actionA,i)
 
 -- | A Kleisli arrow or an ArrowTransformer that can be lifted onto a Kleisli arrow.
-class (Category a,Monad m) => HasMonad m a | a -> m where
-    monadicAction :: (i -> m o) -> a i o
+class (Category a) => HasMonad a where
+    type MonadOf a :: * -> *
+    monadicAction :: (i -> (MonadOf a) o) -> a i o
 
-instance (Monad m) => HasMonad m (Kleisli m) where
+instance (Monad m) => HasMonad (Kleisli m) where
+    type MonadOf (Kleisli m) = m
     monadicAction = Kleisli
 
 -- | A monad that has a Reference type, such as IO and IORef.
