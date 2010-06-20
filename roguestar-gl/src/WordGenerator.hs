9ee7aba1609438f36972ace2d11bb5d203980d73
Christopher Lane Hinson <lane@downstairspeople.org>
2010-06-20 13:49:51 -0400

    Remove really stale license information from one particular file.

--- roguestar-gl/src/WordGenerator.hs
+++ roguestar-gl/src/WordGenerator.hs
@@ -1,23 +1,3 @@
---------------------------------------------------------------------------
---  roguestar-gl: the space-adventure roleplaying game OpenGL frontend.   
---  Copyright (C) 2006 Christopher Lane Hinson <lane@downstairspeople.org>  
---                                                                        
---  This program is free software; you can redistribute it and/or modify  
---  it under the terms of the GNU General Public License as published by  
---  the Free Software Foundation; either version 2 of the License, or     
---  (at your option) any later version.                                   
---                                                                        
---  This program is distributed in the hope that it will be useful,       
---  but WITHOUT ANY WARRANTY; without even the implied warranty of        
---  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         
---  GNU General Public License for more details.                          
---                                                                        
---  You should have received a copy of the GNU General Public License along  
---  with this program; if not, write to the Free Software Foundation, Inc.,  
---  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.           
---                                                                        
---------------------------------------------------------------------------
-
 module WordGenerator
     (HardOrSoft(..),
      WordGenerator(..),
@@ -39,7 +19,7 @@ import Data.Either
 -- patterns.)
 -- 
 data HardOrSoft = Hard
-		| Soft
+                | Soft
 
 -- |
 -- Specifies the hard letter patterns, soft letter patterns,
