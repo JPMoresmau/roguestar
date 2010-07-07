ff403b09777bb392fb9eca0e33accbec15d1a57e
Christopher Lane Hinson <lane@downstairspeople.org>
2010-07-07 14:35:39 -0400

    KinematicSensors.lhs -> .hs

--- rsagl/RSAGL/Animation/KinematicSensors.hs
+++ rsagl/RSAGL/Animation/KinematicSensors.hs
@@ -1,9 +1,3 @@
-\section{Sensors for Inverse Kinematics}
-
-These are various composable modules that provide physics information for the inverse kinematics system.
-
-\begin{code}
-
 {-# LANGUAGE TypeFamilies #-}
 
 module RSAGL.Animation.KinematicSensors
@@ -15,19 +9,19 @@ import RSAGL.Math.Vector
 import RSAGL.FRP
 import RSAGL.Scene.CoordinateSystems
 import RSAGL.Types
-\end{code}
 
-\subsection{Odometer}
-
-The \texttt{odometer} indicates the distance traveled in a remote coordinate system along a vector in the local coordinate system.
-It does not measure side-to-side motion, only motion in the direction of the vector.
-
-\begin{code}
+-- | Measures the distance traveled, by the origin of the local coordinate
+-- system, as measured in a remote coordinate system, in terms
+-- of a vector in the local coordinate system.
+--
+-- For example, if we are animating a model of a wheel, we could put an
+-- odometer at the bottom-most point of the wheel, and then rotate
+-- the wheel according to the result.  The wheel would seem to
+-- turn as it travels, but would not turn when dragged sidewise.
 odometer :: (CoordinateSystemClass s,StateOf m ~ s) => CoordinateSystem -> Vector3D -> FRP e m () RSdouble
 odometer cs measurement_vector_ =
        arr (const origin_point_3d) >>> exportToA cs >>> derivative >>> importFromA cs >>>
        arr (withTime (fromSeconds 1) (dotProduct measurement_vector)) >>>
        integral 0
     where measurement_vector = vectorNormalize measurement_vector_
-\end{code}
 
