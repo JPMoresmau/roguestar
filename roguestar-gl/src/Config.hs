ef0c8f7dbd0ea554c2f0ef17f429490d36c8d6a7
Christopher Lane Hinson <lane@downstairspeople.org>
2010-07-11 21:40:56 -0400

    Add forgotten Config.hs.

---
+++ roguestar-gl/src/Config.hs
@@ -0,0 +1,22 @@
+module Config
+    (short_version_string,
+     version_string,
+     window_name)
+    where
+
+import Paths_roguestar_gl
+import Data.List
+import Data.Version
+
+short_version_string :: String
+short_version_string =
+    (concat $ intersperse "." $ map show $ versionBranch version)
+
+version_string :: String
+version_string = short_version_string ++ if null (versionTags version)
+                     then "" else "[" ++ unwords tags_str ++ "]"
+    where tags_str = intersperse ", " $ map show $ versionTags version
+
+window_name :: String
+window_name = "Roguestar-GL " ++ version_string
+
